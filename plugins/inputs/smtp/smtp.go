a5def6abbe9a8f68d0b93b5ada14b7abc3fbc457
James Buchan <james.buchan@rackspace.com>
2020-02-19 18:56:30 -0700

    Add SMTP Input Plugin

---
+++ plugins/inputs/smtp/smtp.go
@@ -0,0 +1,233 @@
+package smtp
+
+import (
+	"bufio"
+	"errors"
+	"net"
+	"net/textproto"
+	"time"
+
+	"github.com/influxdata/telegraf"
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/plugins/inputs"
+)
+
+type ResultType uint64
+
+const (
+	Success ResultType = iota
+	Timeout
+	ConnectionFailed
+	ReadFailed
+	StringMismatch
+)
+
+// Smtp struct
+type Smtp struct {
+	Address     string
+	Timeout     internal.Duration
+	ReadTimeout internal.Duration
+	Ehlo        string
+	From        string
+	To          string
+	Body        string
+	Tls         bool
+}
+
+var description = "Automates an entire SMTP session and reports metrics"
+
+// Description will return a short string to explain what the plugin does.
+func (*Smtp) Description() string {
+	return description
+}
+
+var sampleConfig = `
+  ## Server address (default localhost)
+  address = "localhost:25"
+
+  ## Set initial connection timeout
+  # timeout = "10s"
+
+  ## Set read timeout
+  # read_timeout = "10s"
+
+  ## Optional value to provide to ehlo command 
+  # ehlo = "example.com"
+
+  ## Optional value to provide to mailfrom command 
+  # from = "me@example.com"
+
+  ## Optional value to provide to rcptto command
+  # to = "you@example.com"
+
+  ## Optional value to provide to data command
+  # body = "this is a test payload"
+
+  ## Optional tls config
+  # tls = false
+`
+
+// SampleConfig will return a complete configuration example with details about each field.
+func (*Smtp) SampleConfig() string {
+	return sampleConfig
+}
+
+func CheckResponse(r *textproto.Reader, operation string, expectedCode int, fields map[string]interface{}, tags map[string]string) (success bool) {
+	var err error
+	code, _, err := r.ReadResponse(expectedCode)
+	if err != nil {
+		if e, ok := err.(net.Error); ok && e.Timeout() {
+			setProtocolMetrics(Timeout, operation, code, fields, tags)
+		} else if e, ok := err.(*textproto.Error); ok && e.Code != 0 {
+			setProtocolMetrics(StringMismatch, operation, code, fields, tags)
+		} else {
+			setProtocolMetrics(ReadFailed, operation, code, fields, tags)
+		}
+		return false
+	}
+	setProtocolMetrics(Success, operation, code, fields, tags)
+	return true
+}
+
+// sends the smtp commands to the server
+// the data command expects an additional input to signal its end
+func WriteCmd(c net.Conn, m string) {
+	c.Write([]byte(m + "\r\n"))
+	if m == "DATA" {
+		c.Write([]byte(".\r\n"))
+	}
+}
+
+// SMTPGather will execute the full smtp session.
+// It will return a map[string]interface{} for fields and a map[string]string for tags
+func (config *Smtp) SMTPGather() (tags map[string]string, fields map[string]interface{}) {
+	// Prepare returns
+	tags = make(map[string]string)
+	fields = make(map[string]interface{})
+	// Start Timer
+	start := time.Now()
+	// Connecting
+	conn, err := net.DialTimeout("tcp", config.Address, config.Timeout.Duration)
+	// Prepare reader
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+	// Stop timer
+	responseTime := time.Since(start).Seconds()
+	fields["connect_time"] = responseTime
+	// Handle error
+	if err != nil {
+		if e, ok := err.(net.Error); ok && e.Timeout() {
+			setResult(Timeout, fields, tags)
+		} else {
+			setResult(ConnectionFailed, fields, tags)
+		}
+		return tags, fields
+	}
+	defer conn.Close()
+
+	// Set the overall read timeout
+	conn.SetDeadline(time.Now().Add(config.ReadTimeout.Duration))
+
+	// Commands are only executed if the previous one was successful
+	success := CheckResponse(tp, "connect", 220, fields, tags)
+	if success && config.Ehlo != "" {
+		WriteCmd(conn, "EHLO "+config.Ehlo)
+		success = CheckResponse(tp, "ehlo", 250, fields, tags)
+	}
+	if success && config.From != "" {
+		WriteCmd(conn, "MAIL FROM:"+config.From)
+		success = CheckResponse(tp, "from", 250, fields, tags)
+	}
+	if success && config.To != "" {
+		WriteCmd(conn, "RCPT TO:"+config.To)
+		success = CheckResponse(tp, "to", 250, fields, tags)
+	}
+	if success && config.Body != "" {
+		WriteCmd(conn, "DATA\r\n"+config.Body)
+		// First check the response from "DATA"
+		success = CheckResponse(tp, "data", 354, fields, tags)
+		if success {
+			// then the response from the body
+			success = CheckResponse(tp, "body", 250, fields, tags)
+		}
+	}
+
+	// Send a quit whether the previous commands succeeded or not
+	WriteCmd(conn, "QUIT")
+	success = CheckResponse(tp, "quit", 221, fields, tags)
+
+	responseTime = time.Since(start).Seconds()
+
+	fields["total_time"] = responseTime
+	return tags, fields
+}
+
+// Gather is called by telegraf when the plugin is executed on its interval.
+// It will call SMTPGather to generate metrics and also fill an Accumulator that is supplied.
+func (smtp *Smtp) Gather(acc telegraf.Accumulator) error {
+	// Set default values
+	if smtp.Timeout.Duration == 0 {
+		smtp.Timeout.Duration = time.Second
+	}
+	if smtp.ReadTimeout.Duration == 0 {
+		smtp.ReadTimeout.Duration = time.Second * 10
+	}
+	// Prepare host and port
+	host, port, err := net.SplitHostPort(smtp.Address)
+	if err != nil {
+		return err
+	}
+	if host == "" {
+		smtp.Address = "localhost:" + port
+	}
+	if port == "" {
+		return errors.New("Bad port")
+	}
+	// Prepare data
+	tags := map[string]string{"server": host, "port": port}
+	var fields map[string]interface{}
+	var returnTags map[string]string
+	// Gather data
+	returnTags, fields = smtp.SMTPGather()
+
+	for key, value := range returnTags {
+		tags[key] = value
+	}
+	// Merge the tags
+	for k, v := range returnTags {
+		tags[k] = v
+	}
+	// Add metrics
+	acc.AddFields("smtp", fields, tags)
+	return nil
+}
+
+func setProtocolMetrics(result ResultType, operation string, foundCode int, fields map[string]interface{}, tags map[string]string) {
+	setResult(result, fields, tags)
+	fields[operation+"_code"] = foundCode
+}
+
+func setResult(result ResultType, fields map[string]interface{}, tags map[string]string) {
+	var tag string
+	switch result {
+	case Success:
+		tag = "success"
+	case Timeout:
+		tag = "timeout"
+	case ConnectionFailed:
+		tag = "connection_failed"
+	case ReadFailed:
+		tag = "read_failed"
+	case StringMismatch:
+		tag = "string_mismatch"
+	}
+
+	tags["result"] = tag
+	fields["result_code"] = uint64(result)
+}
+
+func init() {
+	inputs.Add("smtp", func() telegraf.Input {
+		return &Smtp{}
+	})
+}
---
+++ plugins/inputs/smtp/smtp_test.go
@@ -0,0 +1,284 @@
+package smtp
+
+import (
+	"bufio"
+	"net"
+	"net/textproto"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/influxdata/telegraf/internal"
+	"github.com/influxdata/telegraf/testutil"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+type serverConfig struct {
+	// defines the step at which the connection should close
+	// the connection will close directly before the given step is executed
+	connectionEndPhase ConnectionEndPhase
+}
+
+type ConnectionEndPhase int
+
+const (
+	ConnectionTimeout ConnectionEndPhase = iota + 1
+	Ehlo
+	From
+	To
+	Data
+	Payload
+	Quit
+)
+
+func TestSample(t *testing.T) {
+	c := &Smtp{}
+	output := c.SampleConfig()
+	if output != sampleConfig {
+		t.Error("Sample config doesn't match")
+	}
+}
+
+func TestDescription(t *testing.T) {
+	c := &Smtp{}
+	output := c.Description()
+	if output != description {
+		t.Error("Description output is not correct")
+	}
+}
+
+func TestNoPort(t *testing.T) {
+	var acc testutil.Accumulator
+	c := Smtp{
+		Address: ":",
+	}
+	err1 := c.Gather(&acc)
+	require.Error(t, err1)
+	assert.Equal(t, "Bad port", err1.Error())
+}
+
+func TestAddressOnly(t *testing.T) {
+	var acc testutil.Accumulator
+	c := Smtp{
+		Address: "127.0.0.1",
+	}
+	err1 := c.Gather(&acc)
+	require.Error(t, err1)
+	assert.Equal(t, "address 127.0.0.1: missing port in address", err1.Error())
+}
+
+func TestConnectionError(t *testing.T) {
+	var acc testutil.Accumulator
+	// Init plugin
+	c := getDefaultSmtpConfig()
+	// Error
+	err1 := c.Gather(&acc)
+	for _, p := range acc.Metrics {
+		p.Fields["connect_time"] = 1.0
+	}
+	require.NoError(t, err1)
+	acc.AssertContainsTaggedFields(t,
+		"smtp",
+		map[string]interface{}{
+			"result_code":  uint64(2),
+			"connect_time": 1.0,
+		},
+		map[string]string{
+			"result": "connection_failed",
+			"server": "127.0.0.1",
+			"port":   "2004",
+		},
+	)
+}
+
+func testSmtpHelper(t *testing.T, phase ConnectionEndPhase, fields map[string]interface{}, tags map[string]string) {
+	var wg sync.WaitGroup
+	var acc testutil.Accumulator
+	// Init plugin
+	c := getDefaultSmtpConfig()
+
+	// Start TCP server
+	wg.Add(1)
+	go SmtpServer(t, &wg, serverConfig{
+		connectionEndPhase: phase,
+	})
+	wg.Wait()
+	// Connect
+	wg.Add(1)
+	err1 := c.Gather(&acc)
+	wg.Wait()
+	// Override response time
+	for _, p := range acc.Metrics {
+		p.Fields["connect_time"] = 1.0
+		p.Fields["total_time"] = 2.0
+	}
+	require.NoError(t, err1)
+	acc.AssertContainsTaggedFields(t, "smtp", fields, tags)
+	// Waiting TCPserver
+	wg.Wait()
+}
+
+func TestSmtpFullSession_Success(t *testing.T) {
+	fields, tags := getFieldsAndTags("success", 0, 220, 250, 250, 250, 354, 250, 221)
+	testSmtpHelper(t, 0, fields, tags)
+}
+
+func TestSmtp_FailEhlo(t *testing.T) {
+	fields, tags := getFieldsAndTags("read_failed", 3, 220, 0, 0)
+	testSmtpHelper(t, Ehlo, fields, tags)
+}
+
+func TestSmtp_FailFrom(t *testing.T) {
+	fields, tags := getFieldsAndTags("read_failed", 3, 220, 250, 0, 0)
+	testSmtpHelper(t, From, fields, tags)
+}
+
+func TestSmtp_FailTo(t *testing.T) {
+	fields, tags := getFieldsAndTags("read_failed", 3, 220, 250, 250, 0, 0)
+	testSmtpHelper(t, To, fields, tags)
+}
+
+func TestSmtp_FailData(t *testing.T) {
+	fields, tags := getFieldsAndTags("read_failed", 3, 220, 250, 250, 250, 0, 0)
+	testSmtpHelper(t, Data, fields, tags)
+}
+
+func TestSmtp_FailPayload(t *testing.T) {
+	fields, tags := getFieldsAndTags("read_failed", 3, 220, 250, 250, 250, 354, 0, 0)
+	testSmtpHelper(t, Payload, fields, tags)
+}
+
+// Rather than closing the connection when failing here, we instead get an unexpected response code
+func TestSmtp_FailQuit(t *testing.T) {
+	fields, tags := getFieldsAndTags("string_mismatch", 4, 220, 250, 250, 250, 354, 250, 999)
+	testSmtpHelper(t, Quit, fields, tags)
+}
+
+func TestSmtp_FailTimeout(t *testing.T) {
+	fields, tags := getFieldsAndTags("timeout", 1, 0, 0)
+	testSmtpHelper(t, ConnectionTimeout, fields, tags)
+}
+
+// codes must be provided in the same order as the codeTypes array
+func getFieldsAndTags(status string, result int, codes ...int) (fields map[string]interface{}, tags map[string]string) {
+	codeTypes := []string{
+		"connect_code",
+		"ehlo_code",
+		"from_code",
+		"to_code",
+		"data_code",
+		"body_code",
+	}
+
+	fields = map[string]interface{}{
+		"result_code":  uint64(result),
+		"connect_time": 1.0,
+		"total_time":   2.0,
+	}
+	tags = map[string]string{
+		"result": status,
+		"server": "127.0.0.1",
+		"port":   "2004",
+	}
+
+	// populate the codes provided into the corresponding metric name
+	// codes are only provided if that step is executed
+	// the last code is always for "quit"
+	for i, code := range codes {
+		if i == len(codes)-1 {
+			fields["quit_code"] = code
+		} else {
+			fields[codeTypes[i]] = code
+		}
+
+	}
+
+	return fields, tags
+}
+
+//noinspection GoUnhandledErrorResult
+func SmtpServer(t *testing.T, wg *sync.WaitGroup, resp serverConfig) {
+	tcpAddr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:2004")
+	tcpServer, _ := net.ListenTCP("tcp", tcpAddr)
+	wg.Done()
+	conn, _ := tcpServer.AcceptTCP()
+	reader := bufio.NewReader(conn)
+	tp := textproto.NewReader(reader)
+
+	if resp.connectionEndPhase == ConnectionTimeout {
+		wg.Done()
+		time.Sleep(getDefaultSmtpConfig().ReadTimeout.Duration + 1*time.Second)
+		conn.Close()
+		tcpServer.Close()
+		return
+	}
+
+	conn.Write([]byte("220 myhostname ESMTP Postfix (Ubuntu)\n"))
+
+	for {
+		data, err := tp.ReadLine()
+		if err != nil {
+			t.Error(err)
+			break
+		}
+		if resp.connectionEndPhase == Ehlo {
+			conn.Close()
+			break
+		} else if strings.HasPrefix(data, "EHLO") {
+			conn.Write([]byte("250-myhostname\n"))
+			conn.Write([]byte("250-PIPELINING\n"))
+			conn.Write([]byte("250-SIZE 10240000\n"))
+			conn.Write([]byte("250-VRFY\n"))
+			conn.Write([]byte("250-ETRN\n"))
+			conn.Write([]byte("250-STARTTLS\n"))
+			conn.Write([]byte("250-ENHANCEDSTATUSCODES\n"))
+			conn.Write([]byte("250-8BITMIME\n"))
+			conn.Write([]byte("250-DSN\n"))
+			conn.Write([]byte("250 SMTPUTF8\n"))
+		} else if resp.connectionEndPhase == From {
+			conn.Close()
+			break
+		} else if strings.HasPrefix(data, "MAIL FROM:") {
+			conn.Write([]byte("250 2.1.0 Ok\n"))
+		} else if resp.connectionEndPhase == To {
+			conn.Close()
+			break
+		} else if strings.HasPrefix(data, "RCPT TO:") {
+			conn.Write([]byte("250 2.1.5 Ok\n"))
+		} else if resp.connectionEndPhase == Data {
+			conn.Close()
+			break
+		} else if strings.HasPrefix(data, "DATA") {
+			conn.Write([]byte("354 End data with <CR><LF>.<CR><LF>\n"))
+		} else if resp.connectionEndPhase == Payload {
+			conn.Close()
+			break
+		} else if strings.HasPrefix(data, "testdata") {
+			conn.Write([]byte("250 2.0.0 Ok: queued as C7CAA3F279\n"))
+		} else if resp.connectionEndPhase == Quit {
+			conn.Write([]byte("999 This is a fake error\n"))
+			break
+		} else if strings.HasPrefix(data, "QUIT") {
+			conn.Write([]byte("221 2.0.0 Bye\n"))
+			break
+		}
+	}
+	tcpServer.Close()
+	wg.Done()
+}
+
+func getDefaultSmtpConfig() Smtp {
+	return Smtp{
+		Address:     "127.0.0.1:2004",
+		Timeout:     internal.Duration{Duration: time.Second},
+		ReadTimeout: internal.Duration{Duration: time.Second * 3},
+		Ehlo:        "me@test.com",
+		From:        "me2@test.com",
+		To:          "me3@test.com",
+		Body:        "testdata 12345",
+		Tls:         false,
+	}
+}
